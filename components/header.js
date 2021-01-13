99cf21fcf5170f94f80309893361bd432e3d3643
Naris <naris@odity.fr>
2021-01-13 10:31:29 +0300

    Ajout logo

--- components/header.js
+++ components/header.js
@@ -4,8 +4,11 @@ import SearchForm from './search-form'
 export default function Header() {
   return (
     <div className="h-screen max-h-screen-md bg-hero-pattern">
-      <div className="w-8/12 mx-auto h-full flex justify-center items-center">
+      <div className="w-8/12 mx-auto h-full flex flex-col justify-center items-center">
         <Menu />
+        <div className="mb-8 text-6xl font-extrabold text-white">
+          Logo
+        </div>
         <SearchForm className="w-full shadow-md" />
       </div>  
     </div>
--- components/menu.js
+++ components/menu.js
@@ -3,14 +3,14 @@ import Button from '../components/button'
 export default function Menu(props) {
   return (
     <div className="w-8/12 py-4 absolute top-0 flex justify-between text-white">
-      <Button link className="font-semibold">
+      <Button link className="font-bold">
         Articles
       </Button>
       <div className="flex space-x-4">
-        <Button link className="font-semibold">
+        <Button link className="font-bold">
           S'identifier
         </Button>
-        <Button className="px-6 py-1 font-semibold border-white border-2 rounded-md transition ease-in-out 600 hover:bg-white hover:text-gray-600">
+        <Button className="px-6 py-1 font-bold border-white border-2 rounded-md transition ease-in-out 600 hover:bg-white hover:text-gray-600">
           S'identifier
         </Button>
       </div>
