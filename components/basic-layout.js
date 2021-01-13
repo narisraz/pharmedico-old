8477a964d46fc3e63ac075ce23c47284d0187470
Naris <naris@odity.fr>
2021-01-13 11:09:09 +0300

    Remove div from basic layout

--- components/basic-layout.js
+++ components/basic-layout.js
@@ -6,12 +6,12 @@ export default function BasicLayout(props) {
   } = props
 
   return (
-    <div>
+    <>
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet" />
       </Head>
 
       {children}
-    </div>
+    </>
   )
 }
